2fc0f31ad322eef8e603775f4196ecc29e593d7b
Laptop <micahcorbintanner@gmail.com>
2025-05-14 11:43:30 -0600

    Added more test data

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -7,6 +7,9 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(1, 7, 4, 12)]
+        [InlineData(1, 2, 5, 8)]
+        [InlineData(1, 1, 1, 3)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -27,6 +30,9 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(5, 3, 2)]//Add test data <-------
+        [InlineData(3, 1, 2)]
+        [InlineData(1, 1, 0)]
+        [InlineData(9, 5, 4)]
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -44,6 +50,9 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(6, 5, 30)]//Add test data <-------
+        [InlineData(5, 3, 15)]
+        [InlineData(9, 3, 27)]
+        [InlineData(1, 0, 0)]
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -61,6 +70,9 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(13, 2, 6.5)]//Add test data <-------
+        [InlineData(25, 5, 5)]
+        [InlineData(60, 10, 6)]
+        [InlineData(121, 11, 11)]
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
